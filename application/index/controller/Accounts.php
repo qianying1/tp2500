a9ecda565cbc4572e71cb56886ce5f911a6b9cde
qianying1 <1101627399@qq.com>
2018-01-30 21:25:19 +0800

    Initial commit

---
+++ application/index/controller/Accounts.php
@@ -0,0 +1,93 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+
+class Accounts extends Base
+{
+    //账号列表
+    public function AccountsList()
+    {
+        $admin = $_COOKIE['admin'];
+        return $this->fetch('index/accounts/table',['admin'=>$admin]);
+    }
+    //操作记录
+    public function operateRecord(){
+        return $this->fetch('index/operate/operate_record');
+    }
+    //在线列表
+    public function aliveAccounts(){
+        return $this->fetch('index/accounts/alive_accounts');
+    }
+    //申请送分
+    public function applySendScore(){
+        $sure = input('sure',0,'intval');
+        if($sure==1){
+            $data = input('post.');
+            if(empty($data['account']) || empty($data['score'])){
+                $this->error('内容没填');
+            }else{
+                //进行逻辑操作
+
+            }
+        }else{
+            return $this->fetch('index/accounts/apply_send_score');
+        }
+    }
+    //已删账号列表
+    public function alreadyDel(){
+        return $this->fetch('index/accounts/already_del');
+    }
+    //更改密码
+    public function changePass(){
+        return $this->fetch('index/accounts/change_pass');
+    }
+    //编辑表格-功能设定
+    public function gongnengsheding(){
+        return $this->fetch('index/accounts/gongnengsheding');
+    }
+    //编辑表格-注册送设定
+    public function zhucesongsheding(){
+        return $this->fetch('index/accounts/zhucesongsheding');
+    }
+    //编辑表格-短信送设定
+    public function duanxinsongsheding(){
+        return $this->fetch('index/accounts/duanxinsongsheding');
+    }
+    //编辑表格-统计
+    public function tongji(){
+        return $this->fetch('index/accounts/tongji');
+    }
+    //编辑表格-查账
+    public function chazhang(){
+        $sure = input('sure',0,'intval');
+        if($sure==1){
+            $data = input('post.');
+            return $this->fetch('index/accounts/chazhang',['sure'=>$sure]);
+        }else {
+            return $this->fetch('index/accounts/chazhang',['sure'=>$sure]);
+        }
+    }
+    //编辑表格-曲线
+    public function quxian(){
+        return $this->fetch('index/accounts/quxian');
+    }
+    //编辑表格-禁用
+    public function jinyong(){
+        return $this->fetch('index/accounts/jinyong');
+    }
+    //编辑表格-修改密码
+    public function xiugaimima(){
+        return $this->fetch('index/accounts/xiugaimima');
+    }
+    //
+    public function tableedit(){
+        return $this->fetch('index/accounts/tableedit');
+    }
+    //平台公告
+    public function notice(){
+        return $this->fetch('index/notice');
+    }
+
+
+}
---
+++ application/index/controller/Index.php
@@ -0,0 +1,66 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        if(isset($_COOKIE['admin'])){
+            $sure = input('sure',0,'intval');
+            if($sure==1) {
+                $data = input('post.');
+                return $this->fetch('index/index',['sure'=>$sure]);
+            }else{
+                return $this->fetch('index/index',['sure'=>$sure]);
+            }
+        }else{
+            $this->error('请先登录',url('index/login'));
+        }
+    }
+    //登录
+    public function login(){
+        return $this->fetch('index/login');
+    }
+    //检查登录
+    public function loginCheck(){
+        $data = file_get_contents('index.txt');
+        $arr = explode('&@#',$data);
+        $input = input('post.');
+        if($arr[0]!=$input['Username'] || $arr[1]!=$input['Password']){
+            $this->error('登录失败',url('Index/login'));
+        }else{
+            cookie('admin',$input['Username']);
+            $this->redirect(url('index/index/index'));
+        }
+    }
+    //退出登录
+    public function logout(){
+        cookie('admin',null);
+        $this->redirect('Index/login');
+    }
+    //修改密码
+    public function changePass(){
+        $data = file_get_contents('index.txt');
+        $arr = explode('&@#',$data);
+        $input = input('post.');
+        if(empty($input['old_pass']) && empty($input['new_pass']) || empty($input['re_new_pass'])){
+            $this->error('密码为空',url('Accounts/changePass'));
+        }
+        if($input['old_pass']!=$arr[1]){
+            $this->error('旧密码错误',url('Accounts/changePass'));
+        }
+        if($input['new_pass']!=$input['re_new_pass']){
+            $this->error('两次密码不一致',url('Accounts/changePass'));
+        }
+        if(strlen($input['new_pass'])<6 || strlen($input['new_pass'])>32 || strlen($input['re_new_pass'])<6 || strlen($input['re_new_pass'])>32){
+            $this->error('长度介于6到32之间',url('Accounts/changePass'));
+        }
+        $str = '';
+        $str = $_COOKIE['admin'].'&@#'.$input['new_pass'];
+        file_put_contents('index.txt',$str);
+        $this->success('修改成功',url('Accounts/changePass'));
+    }
+
+}
